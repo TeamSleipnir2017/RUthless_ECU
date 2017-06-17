96c59cba6890fba9d089f25a26b5ed97e9a6b219
jbbjarnason <jbbjarnason@gmail.com>
2017-06-17 12:11:57 +0000

    Made description of debug structure

---
+++ RUthless_ECU/src/include/debug.c
@@ -0,0 +1,9 @@
+﻿/*
+ * debug.c
+ *
+ * Created: 17.6.2017 10:44:10
+ *  Author: Jón Bjarni Bjarnason
+ */ 
+
+#include "debug.h"
+
---
+++ RUthless_ECU/src/include/debug.h
@@ -0,0 +1,53 @@
+﻿/*
+ * debug.h
+ *
+ * Created: 17.6.2017 10:44:31
+ *  Author: Jón Bjarni Bjarnason
+ */ 
+
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+/************************************************************************/
+/* The purpose of this file is to make an efficient debugging way to 
+make debugging a part of the code without taking to much processor power.
+This will be implemented by PDC (Peripheral DMA Controller), and should 
+therefore be able to put the data to serial                             */
+/************************************************************************/
+#define MAX_PACKET_LENGTH 65536 // 16 bit uint
+
+/************************************************************************/
+/* PROGRAM DESCRIPTION
+The program should add data to current packet (which is not transferring)
+and try to send the packet if the PDC is available, if not available 
+return(end).                                                            */
+/************************************************************************/
+
+uint8_t CurrentTransferringPacket; // Should either be 1 or 2
+
+uint8_t Packet1[MAX_PACKET_LENGTH];
+uint8_t Packet2[MAX_PACKET_LENGTH];
+
+uint16_t Packet1Counter; // Number of bytes to send from packet 1
+uint16_t Packet2Counter; // Number of bytes to send from packet 2
+
+/************************************************************************/
+/* The Message format should be "[TIME] STRING VALUE\n"
+	TIME will be in most cases timer counter value of TC8
+	STRING is a character array telling what the value is
+	VALUE can be a 8 - 32 bit unsigned integer
+IF and only IF the buffer (array) is full it should print 
+	"[TIME] FULL !!!!!!\n" and raise a flag
+When the buffer has been addressed to transport PDC, 
+it should lower the flag. If the buffer is full and the PDC is 
+transferring, the data will be discarded.                               */
+/************************************************************************/
+
+void debug_init(Usart * USART_Controller, uint32_t BaudRate);
+
+void debug_add_to_next_message(uint32_t Time, char * String, uint32_t Value); // This can be renamed to more proper name
+
+
+
+#endif /* DEBUG_H_ */
