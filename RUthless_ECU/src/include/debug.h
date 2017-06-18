917344100d21b01b55e889b6239999004370ea94
jbbjarnason <jbbjarnason@gmail.com>
2017-06-18 11:58:34 +0000

    Created structure and new functions

--- RUthless_ECU/src/include/debug.h
+++ RUthless_ECU/src/include/debug.h
@@ -24,13 +24,19 @@ and try to send the packet if the PDC is available, if not available
 return(end).                                                            */
 /************************************************************************/
 
-uint8_t CurrentTransferringPacket; // Should either be 1 or 2
+typedef struct
+{	
+	Usart * UsartPeripheral;
+	
+	uint8_t CurrentTransferringPacket; // Should either be 1 or 2
 
-uint8_t Packet1[MAX_PACKET_LENGTH];
-uint8_t Packet2[MAX_PACKET_LENGTH];
+	uint8_t Packet1[MAX_PACKET_LENGTH];
+	uint8_t Packet2[MAX_PACKET_LENGTH];
 
-uint16_t Packet1Counter; // Number of bytes to send from packet 1
-uint16_t Packet2Counter; // Number of bytes to send from packet 2
+	uint16_t Packet1Counter; // Number of bytes to send from packet 1
+	uint16_t Packet2Counter; // Number of bytes to send from packet 2
+} debug_communication;
+
 
 /************************************************************************/
 /* The Message format should be "[TIME] STRING VALUE\n"
@@ -44,10 +50,16 @@ it should lower the flag. If the buffer is full and the PDC is
 transferring, the data will be discarded.                               */
 /************************************************************************/
 
-void debug_init(Usart * USART_Controller, uint32_t BaudRate);
+void debug_init_usart(debug_communication * Instance, Usart * USART_Controller, uint32_t BaudRate);
 
-void debug_add_to_next_message(uint32_t Time, char * String, uint32_t Value); // This can be renamed to more proper name
+void debug_transfer_new_message(debug_communication * Instance, uint32_t Time, char * String, uint32_t Value);
 
+void debug_add_to_packet(debug_communication * Instance, uint32_t Time, char * String, uint32_t Value, uint8_t * CurrentPacket);
 
+bool debug_is_available(debug_communication * Instance);
+
+void debug_send_message(debug_communication * Instance, uint8_t * CurrentPacket);
+
+void debug_TEST_component();
 
 #endif /* DEBUG_H_ */
